f2c98f7372bfe93540513b2c0e5f8c6af96cd4b7
VinicinBraga <vinicius@bragamatta.com.br>
2021-02-01 21:31:08 -0300

    Exercicios 1 e 2 - Bloco 26.1

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/index.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/index.js
@@ -1,42 +1,37 @@
-const readline = require('readline-sync');
+const inquirer = require('inquirer');
 
-function calculeIMC () {
-  const peso = readline.questionFloat('Qual o seu peso (kg)? ').toFixed(2);
-  const altura = readline.questionFloat('Qual sua altura (m)? ').toFixed(2);
-
-  console.log('Peso: %s, Altura: %s', peso, altura);
-
-  const imc = (peso / Math.pow(altura, 2)).toFixed(2);
-
-  console.log('IMC: %s', imc)
-
-  if (imc < 18.5) {
-    console.log('Situação: Abaixo do peso (magreza)');
-    return;
-  }
-  
-  if (imc >= 18.5 && imc < 25) {
-    console.log('Situação: Peso normal');
-    return;
-  }
-  
-  if (imc >= 25 && imc < 30) {
-    console.log('Situação: Acima do peso (sobrepeso)');
-    return;
-  }
-  
-  if (imc >= 30 && imc < 35) {
-    console.log('Situação: Obesidade grau I');
-    return;
-  }
-  
-  if (imc >= 35 && imc < 40) {
-    console.log('Situação: Obesidade grau II');
-    return;
-  }
-  
-  console.log('Situação: Obesidade graus III e IV');
+function validateFloat (input) {
+  return !isNaN(parseFloat(input)) || 'Por favor, digite um número válido';
 }
 
-calculeIMC()
+async function cauculateIMC () {
+  const answers = await inquirer.prompt([
+    { name: 'peso', type: 'input', message: 'Qual o seu peso (kg)?', validate: validateFloat },
+    { name: 'altura', type: 'input', message: 'Qual a sua altura (m)?', validate: validateFloat }
+  ]);
 
+  const peso = parseFloat(answers.peso);;
+  const altura = parseFloat(answers.altura);
+
+  console.log('peso: %s, altura: %s', peso, altura);
+
+  const imc = (peso / Math.pow(altura, 2)).toFixed(1);
+
+  console.log('IMC: %s', imc);
+
+  if(imc < 18.5) {
+    console.log('Abaixo do peso (magreza)')
+  } else if (imc >=18.5 && imc <= 24.9) {
+    console.log('Peso normal') 
+  } else if  (imc >=25.0 && imc <= 29.9) {
+    console.log('Acima do peso (sobrepeso)') 
+  } else if  (imc >=30.0 && imc <= 34.9) {
+    console.log('Obesidade grau I')
+  } else if  (imc >=35.0 && imc <= 39.9) {s
+    console.log('Obesidade grau II')
+  } else if(imc >= 40.0) { 
+    console.log('Obesidade graus III e IV')   
+  };
+}
+
+cauculateIMC();
